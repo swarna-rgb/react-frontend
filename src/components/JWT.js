8c38bcaeb77682b9f6058e5129c0ed8778296697
Ozal Khan <ozalkhan196@gmail.com>
2020-06-22 14:55:30 -0400

    login ready to implement need to use cookies instead of localStorage for best practices

--- src/components/JWT.js
+++ src/components/JWT.js
@@ -10,27 +10,49 @@ export const AuthView = () => {
   //   helpers
   const fetchToys = async () => {
     try {
-      const { data } = await axios.get('http://localhost:8000/toys/');
-      console.log(data, 'data<><><><><><');
+      let token = localStorage.getItem('token');
+      console.log(token);
+      //   const { data } = await axios.get('http://localhost:8000/toys/');
+      //   console.log(data, 'data<><><><><><');
     } catch (err) {
       console.log(err);
     }
   };
 
-  const handleLogin = (e) => {
+  const changeUser = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async (e, username, password) => {
     e.preventDefault();
     console.log('submited');
+    console.log(username, password, 'data to login');
+    try {
+      const { data } = await axios.post('http://localhost:8000/api/token/', {
+        username,
+        password,
+      });
+      const { access, refresh } = data;
+      localStorage.setItem('token', access);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   //   render
   return (
     <div>
       <h1>Hello world</h1>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={(e) => handleLogin(e, username, password)}>
         <h3>Username</h3>
-        <input type='text' />
+        <input type='text' value={username} onChange={changeUser} />
         <h3>Password</h3>
-        <input type='text' />
+        <input type='text' value={password} onChange={changePassword} />
+        <button type='submit'>Login!</button>
       </form>
       <br />
       <br />
